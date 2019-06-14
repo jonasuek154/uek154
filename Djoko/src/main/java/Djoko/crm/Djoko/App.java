24ec2c2fac7bd1559f4fcab756bcdd5af72a61d6
Jonas Schultheiss <jonas.raphael.schultheiss@gmail.com>
2019-06-14 10:38:11 +0200

    latest

---
+++ Djoko/src/main/java/Djoko/crm/Djoko/App.java
@@ -0,0 +1,13 @@
+package Djoko.crm.Djoko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
